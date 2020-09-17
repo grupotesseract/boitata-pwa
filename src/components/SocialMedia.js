9e294f810106f1b66b8a14d758c4cf0d9a0f055d
kellynvd <kellynvd@gmail.com>
2020-09-17 18:30:17 -0300

    style(SocialMedia): Ajusta tamanho dos icones
    Refs #174516935

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import Icone from './Icone';
-import socialMediaStyles from '../styles/components/socialMedia.module.scss'
+import styles from '../styles/components/socialMedia.module.scss'
 
 const SocialMedia = () => (
   <div>
     <a href='https://www.instagram.com/coletivoboitata/' target='_blank' rel='noreferrer noopener'>
-      <Icone nome='instagram' className={socialMediaStyles.icone} />
+      <Icone nome='instagram' className={styles.icone} />
     </a>
 
     <a href='https://www.facebook.com/coletivoboitata' target='_blank' rel='noreferrer noopener'>
-      <Icone nome='facebook' className={socialMediaStyles.icone} />
+      <Icone nome='facebook' className={styles.icone} />
     </a>
 
     <a href='https://www.behance.net/coletivoboitata' target='_blank' rel='noreferrer noopener'>
-      <Icone nome='behance' className={socialMediaStyles.icone} />
+      <Icone nome='behance' className={styles.icone} />
     </a>
 
     <a href='https://www.youtube.com/channel/UCY955yf9sGoJO1Dmn4R48BQ' target='_blank' rel='noreferrer noopener'>
-      <Icone nome='youtube' className={socialMediaStyles.icone} />
+      <Icone nome='youtube' className={`${styles.icone} ${styles.iconeYoutube}`} />
     </a>
   </div>
 );
