302f4ca7b4b4217f54a8443dcb26e66302093841
Kellyn Duarte <kellynvd@gmail.com>
2020-09-21 20:09:10 -0300

    174516935-contato (#4)
    * feat: Adiciona novo estilo de fonte Raleway
    
    Refs #174516935
    
    * feat(Icone): Adiciona novos icones
    
    Refs #174516935
    
    * feat(contato): Adiciona página de contato
    
    - Adiciona componente TituloContato
    - Adiciona componente FormularioContato
    
    Refs #174516935
    
    * feat: Adiciona componente FaleConoscoRodape
    
    Formulário de contato que é exibido junto ao rodapé do site
    
    Refs #174516935
    
    * style(SocialMedia): Ajusta tamanho dos icones
    
    Refs #174516935
    
    * style: Ajusta style de links e tamanho dos rodapes
    
    Refs #174516935

--- src/components/Icone.js
+++ src/components/Icone.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Behance from '../assets/icons/behance.svg';
+import Email from '../assets/icons/email.svg';
 import Facebook from '../assets/icons/facebook.svg';
+import FacebookRedondo from '../assets/icons/facebook_redondo.svg';
 import Instagram from '../assets/icons/instagram.svg';
+import Telefone from '../assets/icons/telefone.svg';
 import Youtube from '../assets/icons/youtube.svg';
 
 const Icone = ({ nome, className }) => {
   const icones = {
     behance: <Behance className={className} />,
+    email: <Email className={className} />,
     facebook: <Facebook className={className} />,
+    facebookRedondo: <FacebookRedondo className={className} />,
     instagram: <Instagram className={className} />,
+    telefone: <Telefone className={className} />,
     youtube: <Youtube className={className} />,
   };
 
