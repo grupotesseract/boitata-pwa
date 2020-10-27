5cfc7f6d464ac688a447c19450420c1a3abb8f87
kellynvd <kellynvd@gmail.com>
2020-10-27 14:50:43 -0300

    feat(portfolio): Adiciona dados do contentful
    - Adiciona informações dos projetos a partir do contentful
    - Ajusta styles da página e do grid
    
    Refs #175303595

--- src/components/PortfolioGrid.js
+++ src/components/PortfolioGrid.js
@@ -1,108 +1,30 @@
 import React from 'react';
-import { chunk } from 'lodash';
+import { Link } from 'gatsby';
+import { chunk, uniqueId } from 'lodash';
 import styles from '../styles/components/portfolioGrid.module.scss';
 
-const PortfolioGrid = () => {
-  //TODO: Imagens de testes, substituir por dados do Contentful
-  const imagens = [
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/projects/404/d991a7101643141.Y3JvcCwzNDM2LDI2ODcsMCw4.jpg',
-      titulo: 'BODEGA',
-      tags: 'identidade visual, editorial, motion, motion, motion, editorial, testetaggrande'
-    },
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/projects/404/0911cd97182545.Y3JvcCw4MjksNjQ5LDI4NSww.gif',
-      titulo: 'BODEGA',
-      tags: 'identidade visual, editorial, motion'
-    },
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/e1b52279225595.5cbd3fd774ab0.jpg',
-      titulo: 'BODEGA',
-      tags: 'identidade visual, editorial, motion'
-    },
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/projects/404/02543279225595.Y3JvcCw5MTEsNzEzLDQ3NCwyMDU.jpg',
-      titulo: 'FESTIVAL DE ARTES CÊNICAS DE BAURU',
-      tags: 'IDENTIDADE VISUAL, EDITORIAL, MOTION'
-    },
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/projects/404/6c135a80758357.Y3JvcCw5OTQsNzc4LDIwMSww.jpg',
-      titulo: 'BODEGA',
-      tags: 'identidade visual, editorial, motion'
-    },
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/projects/404/02543279225595.Y3JvcCw5MTEsNzEzLDQ3NCwyMDU.jpg',
-      titulo: 'BODEGA',
-      tags: 'identidade visual, editorial, motion'
-    },
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/e1b52279225595.5cbd3fd774ab0.jpg',
-      titulo: 'BODEGA',
-      tags: 'identidade visual, editorial, motion'
-    },
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/projects/404/11104597.5480eb86ae132.jpg',
-      titulo: 'BODEGA',
-      tags: 'identidade visual, editorial, motion'
-    },
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/e1b52279225595.5cbd3fd774ab0.jpg',
-      titulo: 'BODEGA',
-      tags: 'identidade visual, editorial, motion'
-    },
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/e1b52279225595.5cbd3fd774ab0.jpg',
-      titulo: 'BODEGA',
-      tags: 'identidade visual, editorial, motion'
-    },
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/e1b52279225595.5cbd3fd774ab0.jpg',
-      titulo: 'BODEGA',
-      tags: 'identidade visual, editorial, motion'
-    },
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/e1b52279225595.5cbd3fd774ab0.jpg',
-      titulo: 'BODEGA',
-      tags: 'identidade visual, editorial, motion'
-    },
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/e1b52279225595.5cbd3fd774ab0.jpg',
-      titulo: 'BODEGA',
-      tags: 'identidade visual, editorial, motion'
-    },
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/e1b52279225595.5cbd3fd774ab0.jpg',
-      titulo: 'BODEGA',
-      tags: 'identidade visual, editorial, motion'
-    },
-    {
-      src: 'https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/e1b52279225595.5cbd3fd774ab0.jpg',
-      titulo: 'BODEGA',
-      tags: 'identidade visual, editorial, motion'
-    },
-  ];
-
-  const gruposDeImagens = chunk(imagens, 15);
+const PortfolioGrid = ({ projetos }) => {
+  const gruposDeProjetos = chunk(projetos, 15);
 
   return (
     <div className={styles.container}>
       {
-        gruposDeImagens.map((grupoDeImagens, index) => (
-          <div className={styles.portfolio}>
-            {grupoDeImagens.map((imagem, index) => {
+        gruposDeProjetos.map((grupoDeProjeto, index) => (
+          <div key={uniqueId('grupo_de_projeto_')} className={styles.portfolio}>
+            {grupoDeProjeto.map((projeto, index) => {
               const portfolioItemBaseClassName = styles.portfolioItem;
               const portfolioItemNumberClassName = styles[`portfolioItem${index + 1}`];
               const containerClassName = `${portfolioItemBaseClassName} ${portfolioItemNumberClassName}`;
 
               return (
-                <div className={containerClassName}>
-                  <img src={imagem.src} alt="Imagem do Portfólio" className={styles.portfolioImg} />
+                <Link to={`/portfolio/${projeto.slug}`} key={uniqueId('projeto_')} className={containerClassName}>
+                  <img src={projeto.cover} alt="Imagem do Portfólio" className={styles.portfolioImg} />
 
                   <div className={styles.descricao}>
-                    <p className={styles.titulo}>{imagem.titulo}</p>
-                    <p className={styles.tags}>{imagem.tags}</p>
+                    <p className={styles.nome}>{projeto.name}</p>
+                    <p className={styles.categorias}>{projeto.tags.join(', ')}</p>
                   </div>
-                </div>
+                </Link>
               );
             })}
           </div>
